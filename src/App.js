1ec408ec3541b66e29b8c8955f9768ef0da4a4cb
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-11-21 13:36:42 +0100

    Dodano pojedynczy item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
+import OfferList from './components/OfferList/OfferList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <OfferList/>
     </div>
   );
 }
-
 export default App;
