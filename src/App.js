16e4ab44c10273554800d1b027dbc5392ef75f3d
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-11-21 16:10:24 +0100

    add routing and views

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import OfferList from './components/OfferList/OfferList';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
 import './App.css';
 
-import Searching from './components/Searching/Searching';
+import Search from './views/Search';
+import Contact from './views/Contact';
+import Homepage from './views/Homepage';
+import Booking from './views/Booking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div>
-        <Searching/>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <ul className='App-menu'>
+            <li className='App-menu__item'><NavLink to='/'>Start</NavLink></li>
+            <li className='App-menu__item'><NavLink to='search'>Search</NavLink></li>
+            <li className='App-menu__item'><NavLink to='booking'>Booking</NavLink></li>
+            <li className='App-menu__item'><NavLink to='contact'>Contact</NavLink></li>
+          </ul>
+        </header>
+        <div>
+          <Switch>
+              <Route exact path='/' component={Homepage}/>
+              <Route path='/contact' component={Contact}/>
+              <Route path='/search' component={Search}/>
+              <Route path='/booking' component={Booking}/>
+          </Switch>
+        </div>
       </div>
-    <OfferList/>
-    </div>
+    </BrowserRouter>
   );
 }
 export default App;
