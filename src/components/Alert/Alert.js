dc7a353412aa6a71a2749ecb2ea5761e35c5df7f
Dawid Krystochowicz <daw_kryst@interia.pl>
2021-01-05 18:03:50 +0100

    add eslint rules

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import clsx from 'clsx';
-import styles from './Alert.module.scss';
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import styles from "./Alert.module.scss";
 
-function Alert({message, clicked}) {
+function Alert({ message, clicked }) {
   const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
-    setIsActive(true)
-  },[clicked])
+    setIsActive(true);
+  }, [clicked]);
 
   const clickHandler = () => {
     setIsActive(false);
-  }
+  };
 
   return (
-    <div className={clsx(styles.alert, isActive ? styles.active : '')}>
+    <div className={clsx(styles.alert, isActive ? styles.active : "")}>
       <p className={styles.message}>{message}</p>
       <button className={styles.button} onClick={clickHandler}>x</button>
     </div>
-  )
+  );
 }
 
 export default Alert;
