a2410c1b51f12a4207eb819c5d36783d65c12a0a
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2021-01-12 23:52:57 +0100

    poprawiono renderowanie panelu admina

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -9,6 +9,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Modal from "react-modal";
+import api from "../api";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -27,16 +28,24 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 const properData = {
-  login:"admin",
-  password: "admin"
+  login: "admin",
+  password: "admin",
 };
 
-function Admin(props) {
-  const [isModalActive, setIsModalActive]=useState(true);
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
+function Admin() {
+  const [isModalActive, setIsModalActive] = useState(true);
   const [table, setTable] = useState(false);
-  const { bookings } = props;
+  const [bookings, setBookings] = useState([]);
+  const element = "bookings";
+
+  useEffect(() => {
+    let el;
+    api.get(`${element}`).then((response) => {
+      el = response;
+      setBookings(el);
+    });
+  }, [isModalActive]);
+
   /*
   function createData(
     bookingID,
@@ -50,70 +59,67 @@ function Admin(props) {
     return { bookingID, HotelID, name, email, CheckIn, CheckOut, Nights };
   }
   */
-  let tableBody = bookings.map((row) => (
-    <StyledTableRow key={row.name}>
-      <StyledTableCell component="th" scope="row" align="center">
-        {row.bookingID}
-      </StyledTableCell>
-      <StyledTableCell align="center">{row.id}</StyledTableCell>
-      <StyledTableCell align="center">{row.clientID}</StyledTableCell>
-      <StyledTableCell align="center">{row.name}</StyledTableCell>
-      <StyledTableCell align="center">{row.email}</StyledTableCell>
-      <StyledTableCell align="center">{row.CheckIn}</StyledTableCell>
-      <StyledTableCell align="center">{row.CheckOut}</StyledTableCell>
-      <StyledTableCell align="center">{row.Nights}</StyledTableCell>
-    </StyledTableRow>
-  ));
+  let tableBody =
+    bookings &&
+    bookings.map((row) => (
+      <StyledTableRow key={row.name}>
+        <StyledTableCell component="th" scope="row" align="center">
+          {row.bookingID}
+        </StyledTableCell>
+        <StyledTableCell align="center">{row.id}</StyledTableCell>
+        <StyledTableCell align="center">{row.clientID}</StyledTableCell>
+        <StyledTableCell align="center">{row.name}</StyledTableCell>
+        <StyledTableCell align="center">{row.email}</StyledTableCell>
+        <StyledTableCell align="center">{row.CheckIn}</StyledTableCell>
+        <StyledTableCell align="center">{row.CheckOut}</StyledTableCell>
+        <StyledTableCell align="center">{row.Nights}</StyledTableCell>
+      </StyledTableRow>
+    ));
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     const { login: uLogin, password: uPassword } = data;
-    setLogin(uLogin);
-    setPassword(uPassword);
-    if((login===properData.login)&&(password===properData.password))
-    {
-      setTable(true);
+    if((uLogin === properData.login) && (uPassword === properData.password)) {
       setIsModalActive(false);
-    }
-    else {
-
+      setTable(true);
+    } else {
     }
   };
 
   return (
     <div className="Component Admin__Component">
       <Modal className="Admin__Modal" isOpen={isModalActive}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input type="text" placeholder="login" name="login" ref={register} />
           <input
             type="password"
             placeholder="password"
             name="password"
-            ref={register} />
+            ref={register}
+          />
           <input type="submit" />
         </form>
-      
       </Modal>
-        <div className="Admin">
+      <div className="Admin">
         <span className="pageTitle">Admin Panel</span>
-        {table?
-        <TableContainer className="Admin__bookingTable" component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableCell>Booking ID</StyledTableCell>
-              <StyledTableCell align="center">Hotel ID</StyledTableCell>
-              <StyledTableCell align="center">Client ID</StyledTableCell>
-              <StyledTableCell align="center">Name</StyledTableCell>
-              <StyledTableCell align="center">Email</StyledTableCell>
-              <StyledTableCell align="center">Check In </StyledTableCell>
-              <StyledTableCell align="center">Check Out</StyledTableCell>
-              <StyledTableCell align="center">Nights</StyledTableCell>
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>{tableBody}</TableBody>
-        </Table>
-      </TableContainer>
-    :null}
+        {table ? (
+          <TableContainer className="Admin__bookingTable" component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <StyledTableRow>
+                  <StyledTableCell>Booking ID</StyledTableCell>
+                  <StyledTableCell align="center">Hotel ID</StyledTableCell>
+                  <StyledTableCell align="center">Client ID</StyledTableCell>
+                  <StyledTableCell align="center">Name</StyledTableCell>
+                  <StyledTableCell align="center">Email</StyledTableCell>
+                  <StyledTableCell align="center">Check In </StyledTableCell>
+                  <StyledTableCell align="center">Check Out</StyledTableCell>
+                  <StyledTableCell align="center">Nights</StyledTableCell>
+                </StyledTableRow>
+              </TableHead>
+              <TableBody>{tableBody}</TableBody>
+            </Table>
+          </TableContainer>
+        ) : null}
       </div>
     </div>
   );
