eb63910c689e4818a758374681cfa282a7cc52a9
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2021-01-18 23:03:58 +0100

    Zrobiono logowanie

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Button, TextField } from "@material-ui/core";
-import { BrowserRouter, NavLink } from "react-router-dom";
 import api from "../../api";
-function LoginForm() {
-  const element = "users";
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [users, setUsers] = useState([]);
+function LoginForm({ Login }) {
+  const [details, setDetails] = useState({ email: "", password: "" });
   const [error, setError] = useState(false);
-  const handleEmailChange = (e) => { setLogin(e.target.value); setError(false); };
-  const handlePasswordChange = (e) => { setPassword(e.target.value); setError(false); };
-  const handleClick = () => console.log("Wyślij formularz rejestracji");
-  const reset = () => {
-    setError(true);
-    setLogin("");
-    setPassword("");
+  const element = "users";
+  const [users, setUsers] = useState([]);
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    setError(false);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
     const currentUser = users.find(
       (user) =>
-        (user.email === login && user.password === password) ||
-        (user.accountName === login && user.password === password)
+        (user.email === details.email && user.password === details.password) ||
+        (user.accountName === details.email &&
+          user.password === details.password)
     );
-     console.log(currentUser);
-     // eslint-disable-next-line no-unused-expressions
-     currentUser ?null :
-     (reset());
+    if(currentUser) {
+      Login(currentUser);
+    } else {
+      setError(true);
+    }
   };
 
   useEffect(() => {
@@ -40,39 +36,35 @@ function LoginForm() {
   }, []);
 
   return (
-    <BrowserRouter>
     <form className="Login" onSubmit={handleSubmit}>
       <div>
         <TextField
-          name="Login"
-          value={login}
+          name="email"
+          value={details.email}
           label="Login or email"
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$"
-          onChange={handleEmailChange}
-          {... (error && { error: true, helperText:"Incorrect email" }) }
+          onChange={handleChange}
+          {...(error && { error: true, helperText: "Incorrect email" })}
         />
         <TextField
-          value={password}
+          value={details.password}
           type="password"
           name="password"
           label="Password"
           pattern="[A-Za-z0-9]{8,16}"
-          onChange={handlePasswordChange}
-          {... (error && { error: true, helperText:"Incorrect password" }) }
+          onChange={handleChange}
+          {...(error && { error: true, helperText: "Incorrect password" })}
         />
       </div>
       <Button
         type="submit"
-        onClick={handleClick}
         variant="outlined"
         color="default"
         onSubmit={handleSubmit}
       >
         Sign in
       </Button>
-      <NavLink to="/user">User</NavLink>
     </form>
-    </BrowserRouter>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/LoginForm/LoginForm";
-
+import { Button } from "@material-ui/core";
 function Login() {
+  const Login = details => {
+    console.log(details);
+    setUser({
+      email: details.email,
+      password: details.password
+    });
+  };
+  const Logout = () => {
+    setUser({
+      email: "",
+      password: ""
+    });
+  };
+  const [user, setUser] = useState({ email:"", password: "" });
   return (
-    <LoginForm/>
+    <div>
+      {user.email !== "" ? (
+      <div>
+        Welcome {user.email}
+        <Button
+        onClick={Logout}
+        variant="outlined"
+        color="default"
+      >
+     Logout
+      </Button>
+      </div>
+      
+      ):<LoginForm Login = {Login}/>}
+    </div>
+
   );
 }
 
