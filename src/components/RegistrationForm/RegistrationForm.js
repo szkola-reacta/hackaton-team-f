80fad5b56e16449b1dc6c0f38814628459039333
Dorota Poreba-Polomska <dorota@fresha.com>
2021-01-20 19:25:04 +0100

    repair pattern on forms Contact and Registration

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, TextField } from "@material-ui/core";
+
+
 function RegistrationForm() {
   return (
     <form className="Registration">
@@ -8,39 +10,36 @@ function RegistrationForm() {
         required
         name="name"
         label="Name"
-        pattern="[A-Za-z]{2,99}"
+        inputProps={{ pattern: "[A-Za-z\\s]{2,99}" }}
       />
       <TextField
         required
         name="surname"
         label="Surname"
-        pattern="[A-Za-z]{2,99}"
+        inputProps={{ pattern: "[A-Za-z\\s]{2,99}" }}
       />
-      <TextField
+      <TextField required
         name="email"
-        label="Email"
-        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$"
+        label="Email" inputProps={{ pattern: "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$" }}
       />
       <TextField
         name="password"
         label="Password"
+        type="password"
         pattern="[A-Za-z0-9]{8,16}"
+        inputProps={{ pattern: "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$" }}
       />
       <TextField
         name="account_name"
         label="Account name"
-        pattern="[A-Za-z]{2,99}"
+        inputProps={{ pattern: "^[a-zA-Z0-9_.-]*$" }}
       />
-      <TextField name="address" label="Address" />
-      <TextField
-        name="postcode"
-        label="Postcode"
-      />
+      <TextField name="address" label="Address" inputProps={{ pattern: "^[a-zA-Z0-9_.-\\s]*$" }}/>
       <TextField
         type="number"
         name="phone"
         label="Phone"
-        pattern="[0-9]{3}[0-9]{3}[0-9]{3}"
+        inputProps={{ pattern: "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$" }}
       />
         </div>
    
